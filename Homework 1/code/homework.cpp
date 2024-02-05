f78c798ef5801a48d3e6afcb67355542b4ff423a
Srivarshan Selvaraj <srivarshan02@gmail.com>
2024-02-05 10:13:33 -0800

    Add A* search

--- Homework 1/code/homework.cpp
+++ Homework 1/code/homework.cpp
@@ -20,6 +20,7 @@ struct Node
     int parent;
     int momentum;
     float path_len;
+    float estm_total_path_len;
 };
 
 // Custom comparator for UCS priority queue
@@ -28,6 +29,15 @@ struct UcsNodeComparator
     bool operator()(const Node &a, const Node &b)
     {
         return (a.path_len > b.path_len);
+    }
+};
+
+// Custom comparator for A* priority queue
+struct AStarNodeComparator
+{
+    bool operator()(const Node &a, const Node &b)
+    {
+        return (a.estm_total_path_len > b.estm_total_path_len);
     }
 };
 
@@ -254,6 +264,10 @@ double euc_dist(int x1, int y1, int x2, int y2)
 {
     return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 }
+double euc_dist(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2) + pow((z2 - z1), 2));
+}
 
 // Function to perform uniform-cost search
 void ucs(vector<Node> &node_list, map<string, int> &node_num_map, int energy_limit, Output &output)
@@ -379,6 +393,143 @@ void ucs(vector<Node> &node_list, map<string, int> &node_num_map, int energy_lim
     cout << " sec " << endl;
 }
 
+// Evaluation function for heuristic search
+float eval_func(Node &n1, Node &n2)
+{
+    return euc_dist(n1.x , n1.y, n1.z, n2.x , n2.y, n2.z);
+}
+
+// Function to perform a* search
+void a_star(vector<Node> &node_list, map<string, int> &node_num_map, int energy_limit, Output &output)
+{
+    // Initialize clock functions
+    clock_t start, end;
+    vector<double> open_time, close_time;
+
+    // Initialize open priority queue
+    priority_queue<Node, vector<Node>, AStarNodeComparator> open;
+    // Initialize map for open queue
+    map< int, vector<Node> > open_map;
+
+    // Initialize visited array
+    vector<Node> visited;
+    // Initialize map for visited array
+    map< int, vector<int> > visited_map;
+
+    // Get the goal node for the heuristic function
+    Node goal_node = node_list[node_num_map["goal"]];
+
+    // Add start node to open queue
+    Node start_node = node_list[node_num_map["start"]];
+    start_node.parent = -1;
+    start_node.path_len = 0;
+    start_node.estm_total_path_len = eval_func(start_node, goal_node);
+    start_node.momentum = 0;
+    open.push(start_node);
+    open_map[start_node.id].push_back(start_node);
+
+    // Main loop
+    while (true)
+    {
+        // If open queue is empty then return FAIL
+        if (open.empty())
+        {
+            output.output_file << "FAIL" << endl;
+            cout << "FAIL" << endl;
+            output.pathlen_file << -1 << endl;
+            cout << "Path lenght: " << -1 << endl;
+            break;
+        }
+
+        // Get the front node from open priority queue
+        Node curr_node = open.top();
+        open.pop();
+
+        // Add node to visited array
+        visited.push_back(curr_node);
+        // Add to visited map
+        visited_map[curr_node.id].push_back(visited.size() - 1);
+
+        // If we have reached goal state then return path
+        if (curr_node.name == "goal")
+        {
+            cout << "Goal reached!" << endl;
+            print_path(visited, curr_node, output);
+            break;
+        }
+
+        // Expand the current node
+        vector<int> children = curr_node.neighbors;
+        for (int i = 0; i < children.size(); i++)
+        {
+            int child = children[i];
+            int parent_idx = visited.size() - 1;
+
+            // Get the child node
+            Node child_node = node_list[child];
+            
+            // Calculate momentum for the child
+            child_node.momentum = max(0, curr_node.z - child_node.z);
+
+            // Compute path length for the child
+            child_node.path_len = curr_node.path_len + euc_dist(curr_node.x, curr_node.y, curr_node.z, child_node.x, child_node.y, child_node.z);
+
+            // Check if child is in visited array
+            start = clock();
+            bool x = contains_ucs(visited, visited_map, child, parent_idx, child_node.momentum, child_node.path_len);
+            end = clock();
+            double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+            close_time.push_back(time_taken);
+            if (x)
+                continue;
+
+            // Check if child is in open priority queue
+            start = clock();
+            x = contains_ucs(open_map, child, parent_idx, child_node.momentum, child_node.path_len);
+            end = clock();
+            time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+            open_time.push_back(time_taken);
+            if (x)
+                continue;
+
+            // Check if child is reachable
+            int energy_req = child_node.z - curr_node.z;
+            if (curr_node.momentum + energy_limit < energy_req)
+                continue;
+
+            // Compute parent of the child
+            child_node.parent = parent_idx;
+
+            // Compute estimated total path length
+            child_node.estm_total_path_len = child_node.path_len + eval_func(child_node, goal_node);
+
+            // Add child to open queue
+            open.push(child_node);
+            open_map[child_node.id].push_back(child_node);
+        }
+    }
+
+    double time_taken_open = 0;
+    for (int i = 0; i < open_time.size(); i++)
+    {
+        time_taken_open += open_time[i];
+    }
+
+    double time_taken_close = 0;
+    for (int i = 0; i < close_time.size(); i++)
+    {
+        time_taken_close += close_time[i];
+    }
+
+    cout << endl;
+    cout << "Total time taken for open queue is : " << fixed
+         << time_taken_open << setprecision(5);
+    cout << " sec " << endl;
+    cout << "Total time taken for close array is : " << fixed
+         << time_taken_close << setprecision(5);
+    cout << " sec " << endl;
+}
+
 int main()
 {
     // Read the input file
@@ -441,39 +592,28 @@ int main()
     input_file.close();
 
     // // Initialize the output struct
-    // Output output;
-    // output.output_file.open("output.txt");
-    // output.pathlen_file.open("pathlen.txt");
+    Output output;
+    output.output_file.open("output.txt");
+    output.pathlen_file.open("pathlen.txt");
 
     // Perform search algorithm according to search type
     if (search_type == "BFS")
     {
-        // Initialize the output struct
-        Output output;
-        output.output_file.open("output.txt");
-        output.pathlen_file.open("pathlen.txt");
-
         bfs(node_list, node_num_map, rover_energy, output);
-
-        output.output_file.close();
-        output.pathlen_file.close();
     }
     else if (search_type == "UCS")
     {
-        // Initialize the output struct
-        Output output;
-        output.output_file.open("output.txt");
-        output.pathlen_file.open("pathlen.txt");
-
         ucs(node_list, node_num_map, rover_energy, output);
+    }
+    else if (search_type == "A*")
+    {
+        a_star(node_list, node_num_map, rover_energy, output);
 
-        output.output_file.close();
-        output.pathlen_file.close();
     }
 
     // Close the files in output struct
-    // output.output_file.close();
-    // output.pathlen_file.close();
+    output.output_file.close();
+    output.pathlen_file.close();
 
     return 0;
 }
