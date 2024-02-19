f8d47c26a132bfaa8185d2b8b4d36d2de881d51c
Srivarshan Selvaraj <srivarshan02@gmail.com>
2024-02-19 12:18:50 -0800

    Add GameState class

--- Homework 2/code/main.cpp
+++ Homework 2/code/main.cpp
@@ -4,20 +4,34 @@
 
 using namespace std;
 
-// Function to print the state of the board
-void print_board(vector<vector<char>> &board)
+// Global constant to declare the board size
+const int BOARD_SIZE = 12;
+
+// Class to maintain game state
+class GameState
 {
-    for (int i = 0; i < board.size(); i++)
+private:
+    vector<vector<char>> board;
+
+public:
+    GameState(vector<vector<char>> &board)
     {
-        vector<char> board_line = board[i];
-        for (int j = 0; j < board_line.size(); j++)
-        {
-            cout << board_line[j] << "    ";
-        }
-        cout << "\n\n";
+        this->board = board;
     }
-    
-}
+    // Function to print the state of the board
+    void print_board()
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            vector<char> board_line = board[i];
+            for (int j = 0; j < board_line.size(); j++)
+            {
+                cout << board_line[j] << "    ";
+            }
+            cout << "\n\n";
+        }
+    }
+};
 
 int main()
 {
@@ -37,19 +51,19 @@ int main()
     iss >> player_time >> opponent_time;
 
     // Read the state of the board
-    vector<vector<char>> board(12);
-    for (int i = 0; i < 12; ++i)
+    vector<vector<char>> board;
+    for (int i = 0; i < BOARD_SIZE; ++i)
     {
         string board_line_str;
         getline(input_file, board_line_str);
-        vector<char> board_line(12);
-        for (int j = 0; j < 12; j++)
+        vector<char> board_line;
+        for (int j = 0; j < BOARD_SIZE; j++)
         {
             board_line.push_back(board_line_str[j]);
         }
         board.push_back(board_line);
     }
-    // print_board(board);
-
+    GameState start_state(board);
+    start_state.print_board();
     return 0;
 }
